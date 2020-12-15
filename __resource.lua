0087d9712e1c22f2d478fcfce5fe8cdc1fcdf543
Nolfi <68621653+Nolfi24@users.noreply.github.com>
2020-12-15 18:47:26 +0100

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+description 'Made by Nolfi | Github.com/Nolfi24'
+
+files {
+    'index.html',
+    'style.css',
+    'js/main.js',
+    'js/progressbar.js',
+    'js/typed.js',
+}
+
+loadscreen 'index.html'
